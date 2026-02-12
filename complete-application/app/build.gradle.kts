5edaca2d6d9032a43cfc41d7aa7ac5d0421b355f
Mike Rudat <mike.rudat@fusionauth.io>
2026-02-12 11:11:53 -0700

    Use Android SDK v1.0.0

--- complete-application/app/build.gradle.kts
+++ complete-application/app/build.gradle.kts
@@ -53,7 +53,7 @@ dependencies {
     implementation("androidx.core:core-ktx:1.17.0")
     implementation("androidx.appcompat:appcompat:1.7.1")
     implementation("com.google.android.material:material:1.13.0")
-    implementation("io.fusionauth:fusionauth-android-sdk:1.0.0-rc")
+    implementation("io.fusionauth:fusionauth-android-sdk:1.0.0")
     implementation("androidx.constraintlayout:constraintlayout:2.2.1")
     implementation("androidx.navigation:navigation-fragment-ktx:2.9.6")
     implementation("androidx.navigation:navigation-ui-ktx:2.9.6")
